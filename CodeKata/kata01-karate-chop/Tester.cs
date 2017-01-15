d031acafee6180c44c52acdf3a4dbf4c0bad47c6
tcadmin <tcadmin@TC-LAPTOP>
2017-01-15 09:20:03 +0000

    Second Pass Failure

--- CodeKata/kata01-karate-chop/Tester.cs
+++ CodeKata/kata01-karate-chop/Tester.cs
@@ -8,50 +8,122 @@
     [TestFixture]
     public class Tester
     {
-        [Test, TestCaseSource(typeof(Kata01TastCaseData), nameof(Kata01TastCaseData.TestCases))]
-        public int Chop(int integer, int[] arrayOfIntegers)
-        {
-            var result = -1;
+        private const int NotFound = -1;
 
+        [Test, TestCaseSource(typeof(Kata01TastCaseData), nameof(Kata01TastCaseData.TestCases))]
+        public int Chopv3(int integer, int[] arrayOfIntegers)
+        {
             if (arrayOfIntegers == null || arrayOfIntegers.Length == 0)
             {
-                return result;
+                return NotFound;
             }
 
-            const int StartOfSearch = 0;
-            result = this.Chop(integer, arrayOfIntegers, StartOfSearch, result);
-
-            return result;
+            return this.Chop(integer, arrayOfIntegers, arrayOfIntegers.Length);
         }
 
-        private int Chop(int integer, int[] arrayOfIntegers, int startOfSearch, int result)
+        private int Chop(int integer, int[] arrayOfIntegers, int arrayLength, int lastChopEnd = 0)
         {
+            int maxPosition = arrayLength - 1;
+            int chopStart = lastChopEnd;
+            int maxChopLength = maxPosition - chopStart;
+            double midChopPosition = (double)maxChopLength / 2;
 
-            var length = arrayOfIntegers.Length;
-            var maxIndex = length - 1;
+            int chopEnd = (int)Math.Round(midChopPosition, MidpointRounding.AwayFromZero);
 
-            var chopPoint = (decimal)(((decimal)maxIndex - (decimal)startOfSearch) / 2);
-
-            var chopIndex = (int)Math.Round(chopPoint, 0, MidpointRounding.AwayFromZero);
-
-            for (var i = startOfSearch; i <= chopIndex; i++)
+            for (int position = chopStart; position <= chopEnd; position++)
             {
-                if (arrayOfIntegers[i] != integer)
+                if (arrayOfIntegers[position] == integer)
                 {
-                    continue;
+                    return position;
                 }
-
-                result = i;
-                return result;
             }
 
-            if (maxIndex > chopIndex)
+            if (chopEnd < maxPosition)
             {
-                this.Chop(integer, arrayOfIntegers, chopIndex, result);
+                return this.Chop(integer, arrayOfIntegers, arrayLength, chopEnd);
             }
 
-            return result;
+            return NotFound;
         }
+
+        //public int Chopv2(int integer, int[] arrayOfIntegers)
+        //{
+        //    if (arrayOfIntegers == null || arrayOfIntegers.Length == 0)
+        //    {
+        //        return NotFound;
+        //    }
+
+        //    return this.Chop(integer, arrayOfIntegers, arrayOfIntegers.Length);
+        //}
+
+        //private int Chopv2(int integer, int[] arrayOfIntegers, int arrayLength, int lastChopEnd = 0)
+        //{
+        //    int maxPosition = arrayLength - 1;
+        //    int chopStart = lastChopEnd;
+        //    int maxChopLength = maxPosition - chopStart;
+        //    double midChopPosition = (double)maxChopLength / 2;
+
+        //    int chopEnd = (int)Math.Round(midChopPosition, MidpointRounding.AwayFromZero);
+
+        //    for (int position = chopStart; position <= chopEnd; position++)
+        //    {
+        //        if (arrayOfIntegers[position] == integer)
+        //        {
+        //            return position;
+        //        }
+        //    }
+
+        //    if (chopEnd < maxPosition)
+        //    {
+        //        return this.Chop(integer, arrayOfIntegers, arrayLength, chopEnd);
+        //    }
+
+        //    return NotFound;
+        //}
+
+        //public int Chopv1(int integer, int[] arrayOfIntegers)
+        //{
+        //    var result = -1;
+
+        //    if (arrayOfIntegers == null || arrayOfIntegers.Length == 0)
+        //    {
+        //        return result;
+        //    }
+
+        //    const int StartOfSearch = 0;
+        //    result = this.Chop(integer, arrayOfIntegers, StartOfSearch, result);
+
+        //    return result;
+        //}
+
+        //private int Chopv1(int integer, int[] arrayOfIntegers, int startOfSearch, int result)
+        //{
+
+        //    var length = arrayOfIntegers.Length;
+        //    var maxIndex = length - 1;
+
+        //    var chopPoint = (decimal)(((decimal)maxIndex - (decimal)startOfSearch) / 2);
+
+        //    var chopIndex = (int)Math.Round(chopPoint, 0, MidpointRounding.AwayFromZero);
+
+        //    for (var i = startOfSearch; i <= chopIndex; i++)
+        //    {
+        //        if (arrayOfIntegers[i] != integer)
+        //        {
+        //            continue;
+        //        }
+
+        //        result = i;
+        //        return result;
+        //    }
+
+        //    if (maxIndex > chopIndex)
+        //    {
+        //        this.Chop(integer, arrayOfIntegers, chopIndex, result);
+        //    }
+
+        //    return result;
+        //}
     }
 
     public class Kata01TastCaseData
